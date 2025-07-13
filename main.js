6519194ad0c2c024469161fadeb73f894256b12e
KM Zikhali <kwanele.zikhali.2002@gmail.com>
2025-07-13 07:52:56 +0200

    Initial commit

---
+++ main.js
@@ -0,0 +1,15 @@
+let ctors;
+
+ctors = {
+	copy2xa: val=> void 0,
+	copy2bx: val => void 0,
+	copy2cx: val => void 0,
+	copy2sp: ()=> void 0,
+	biosinterrupt: () => void 0,
+	interuptoff: () => void 0,
+	halt: () => void 0,
+	jmp: () => void 0,
+	padding: amt => void 0,
+	magic: () => void 0
+};
+
---
+++ temp.asm
@@ -0,0 +1,6 @@
+bits 16
+
+mov ax,0xaabb
+mov bx,0xaabb
+mov cx,0xaabb
+mov ap,ax
