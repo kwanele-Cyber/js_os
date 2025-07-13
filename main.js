60f70c67e644325f05ad91a900b74e10c820aaf8
KM Zikhali <kwanele.zikhali.2002@gmail.com>
2025-07-13 09:07:33 +0200

    implemented copy2ax(val)

--- main.js
+++ main.js
@@ -1,7 +1,25 @@
 let ctors;
 
+let rev;
+
+rev = val => {
+	let a , b;
+	let mask;
+	let i;
+
+	mask = 0xff;
+	a = (val & mask);
+	mask =0xff00;
+	i = (val & mask);
+	b = (i >> 8);
+
+	return String.fromCharCode(a)
+	    .concat(String.fromCharCode(b));
+};
+
+// b8 bb b9
 ctors = {
-	copy2xa: val=> void 0,
+	copy2ax: val=> "\xb8"+rev(val),
 	copy2bx: val => void 0,
 	copy2cx: val => void 0,
 	copy2sp: ()=> void 0,
@@ -13,3 +31,10 @@ ctors = {
 	magic: () => void 0
 };
 
+let x, y;
+x = 0xccdd;
+y = ctors.copy2ax(x);
+
+console.log(y);
+
+console.log(y);
